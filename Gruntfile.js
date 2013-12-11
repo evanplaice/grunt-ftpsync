05dede124dc489b929487aa2658b6508a757c7bf
Evan Plaice <evanplaice@gmail.com>
2013-12-11 03:30:52 -0800

    Modified gruntfile.js for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,31 @@
 'use strict';
 
 module.exports = function(grunt) {
-
   // Project configuration.
   grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
+    //nodeunit: {
+    //  files: ['test/**/*_test.js'],
+    //},
     jshint: {
       options: {
-        jshintrc: '.jshintrc'
+        "curly": true,
+        "eqeqeq": true,
+        "immed": true,
+        "latedef": "nofunc",
+        "newcap": true,
+        "noarg": true,
+        "sub": true,
+        "undef": true,
+        "unused": true,
+        "boss": true,
+        "eqnull": true,
+        "node": true
       },
       gruntfile: {
-        src: 'Gruntfile.js'
+        src: 'gruntfile.js'
       },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
+      tasks: {
+        src: ['tasks/**/*.js']
       },
     },
     watch: {
@@ -26,23 +33,36 @@ module.exports = function(grunt) {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
       },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
+      tasks: {
+        files: '<%= jshint.tasks.src %>',
+        tasks: ['jshint:tasks', 'nodeunit']
       },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
+      //test: {
+      //  files: '<%= jshint.test.src %>',
+      //  tasks: ['jshint:test', 'nodeunit']
+      //},
+    },
+    ftpsync: {
+      local: 'C:/Users/eplaice/Dev/node-ftpsync/test/local/',
+      remote: '/',
+      host: "127.0.0.1",
+      port: 9999,
+      user: "anonymous",
+      pass: "",
+      connections: 1,
+      ignore: [
+        ".htaccess"
+      ]
     },
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  //grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadTasks('./tasks');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  grunt.registerTask('default', ['jshint']);
 
 };
